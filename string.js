6781947f23bc3ab0b74abb15d2f4f387ed1743c5
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2019-12-10 16:57:35 +0200

    string

---
+++ string.js
@@ -0,0 +1,32 @@
+let ucFirst = (str) => {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+let checkSpam = (str) => {
+  let temp = str.toLowerCase();
+  return temp.includes("viagra") || temp.includes("xxx");
+}
+
+let truncate = (str, maxlength) => {
+  if(str.length > maxlength){
+    return str.slice(0, maxlength) + "...";
+  }
+  else {
+    return str;
+  }
+}
+
+let extractCurrencyValue = (str) => {
+  if(str.includes("$")){
+     return +str.slice(1);
+  }
+  else {
+    return 0
+  }
+}
+
+console.log(ucFirst("kkk"));
+console.log(checkSpam("XXX"));
+console.log(truncate("fsdafdfsdaf", 4));
+console.log(extractCurrencyValue("$9"));
+
