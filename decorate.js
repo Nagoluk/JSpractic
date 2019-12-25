334ea608eae7035e1d0d7a6e2fcb9813ee42f5b5
Nagoluk <51786414+Nagoluk@users.noreply.github.com>
2019-12-25 13:47:32 +0200

    decorator

--- decorate.js
+++ decorate.js
@@ -27,3 +27,23 @@ for (let args of work.calls) {
   console.log( 'call:' + args.join() ); // "call:1,2", "call:4,5"
 }
 
+//Создайте декоратор delay(f, ms), 
+//который задерживает каждый вызов f на ms миллисекунд.
+
+function f(x) {
+  console.log(x);
+}
+
+function delay(fc, time){
+  return function () {
+    setTimeout(()=> fc.apply(this, arguments), time);
+  }
+}
+
+// создаём обёртки
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+
+f1000("test"); // показывает "test" после 1000 мс
+f1500("test"); // показывает "test" после 1500 мс
+
